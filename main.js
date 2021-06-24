a03de4cb3a455327189ebe50b97eea4074a197dc
Abhijeet Saxena <saxenaabhijeet@outlook.com>
2021-06-24 18:29:59 +0530

    chore(app): Scaffold project using vitejs

---
+++ main.js
@@ -0,0 +1,8 @@
+import "./style.css";
+import { test } from "./Minesweeper";
+
+document.querySelector("#app").innerHTML = `
+  <h1>Hello Vite!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`;
+test();
