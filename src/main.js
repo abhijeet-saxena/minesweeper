3901e51671eff986836d8f804c0e411ea8c19081
Abhijeet Saxena <saxenaabhijeet@outlook.com>
2021-06-25 21:30:57 +0530

    feat: Render minecraft board

--- src/main.js
+++ src/main.js
@@ -1,12 +1,29 @@
 import "./styles/style.css";
 import Minesweeper from "./scripts/Minesweeper";
 
-document.querySelector("#app").innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+const ROWS = 10;
+const COLUMNS = 10;
 
-const game = new Minesweeper(3, 3, 2);
+const board = document.querySelector("#board");
+
+const colorMap = {};
+
+const game = new Minesweeper(ROWS, COLUMNS, 21);
 game.init();
 game.printBoard();
-console.log(game.board);
+// console.log(game.board);
+
+const generateBoard = (arr) => {
+  // console.log(arr);
+  let op = "";
+
+  for (let cell of arr) {
+    console.log(cell);
+    op += `<div class="cell">${cell.value}</div>`;
+  }
+  // board.style.width =
+
+  board.innerHTML = op;
+};
+
+generateBoard(game.board.flat());
